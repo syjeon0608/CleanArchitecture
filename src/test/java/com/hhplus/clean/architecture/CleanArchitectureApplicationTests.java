5c13b08df389d632d401e167b54133dd673b11ea
syjeon0608 <syjeon0608@gmail.com>
2024-10-01 21:30:48 +0900

    Initial commit

---
+++ src/test/java/com/hhplus/clean/architecture/CleanArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhplus.clean.architecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
