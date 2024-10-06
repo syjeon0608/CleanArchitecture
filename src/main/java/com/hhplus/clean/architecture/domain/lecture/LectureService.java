11e6801c7dc139bc8a11247baa021bb86a6f621f
syjeon0608 <syjeon0608@gmail.com>
2024-10-06 14:43:31 +0900

    chore: import 정리

--- src/main/java/com/hhplus/clean/architecture/domain/lecture/LectureService.java
+++ src/main/java/com/hhplus/clean/architecture/domain/lecture/LectureService.java
@@ -9,7 +9,6 @@ import com.hhplus.clean.architecture.domain.user.User;
 import com.hhplus.clean.architecture.domain.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.HashMap;
